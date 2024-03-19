fdfc8e108b982fcf6b375adf3b04644a49c2fec0
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-19 05:54:57 -0700

    :zap:

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-/* Creates a new user and a new database */
+/*Creates a new user and a new database*/
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
