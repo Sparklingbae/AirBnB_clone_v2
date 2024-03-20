757d4b788594e899b3885f747db6e35395f9c2ef
Hucksapp <hucksapp@gmail.com>
2024-03-20 16:30:15 -0700

    :zap:

--- models/place.py
+++ models/place.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the place class"""
-from sqlalchemy.ext.declarative import declarative_base
+
 from models.base_model import BaseModel, Base
+from models.amenity import  Amenity
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
